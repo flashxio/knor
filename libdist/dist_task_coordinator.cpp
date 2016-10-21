4a0fa52ee728b2e045cb9f4c7fe6f5ac428de395
Disa Mhembere <dmhembe1@jhu.edu>
2016-10-21 18:26:07 -0400

    dist: data partition with mpi sucessful

--- libdist/dist_task_coordinator.cpp
+++ libdist/dist_task_coordinator.cpp
@@ -24,7 +24,6 @@
 #include "dist_task_coordinator.hpp"
 #include "dist_task_thread.hpp"
 #include "clusters.hpp"
-#include "mpi.h"
 
 namespace kpmeans { namespace prune {
 
@@ -32,29 +31,57 @@ dist_task_coordinator::dist_task_coordinator(
         const std::string fn, const size_t nrow,
         const size_t ncol, const unsigned k, const unsigned max_iters,
         const unsigned nnodes, const unsigned nthreads,
+        const unsigned mpi_rank, const unsigned nprocs,
         const double* centers, const kpmbase::init_type_t it,
         const double tolerance, const kpmbase::dist_type_t dt) :
-    kmeans_task_coordinator(fn, nrow, ncol, k, max_iters,
-            nnodes, nthreads, centers, it, tolerance, dt) {
+    kmeans_task_coordinator(fn, get_proc_rows(nrow, nprocs, mpi_rank),
+            ncol, k, max_iters, nnodes, nthreads, centers, it, tolerance, dt) {
 
-        // TODO
-    }
+        this->mpi_rank = mpi_rank;
+        this->nprocs = nprocs;
 
-void dist_task_coordinator::random_partition_init() {
-    // TODO
+        for (thread_iter it = threads.begin(); it < threads.end(); ++it)
+            (*it)->set_start_rid((*it)->get_start_rid()
+                    + (nrow / nprocs) * mpi_rank);
 }
 
-void kmeanspp_init() {}
-void forgy_init() {}
-void run_kmeans() {}
-
 /**
- * Main driver for kmeans
- */
-void kmeans_task_coordinator::run_kmeans() {}
+  * This takes the global number of samples in the *entire* dataset, `g_nrow'
+  *     and gives the coordinator it's partion.
+  */
+size_t const dist_task_coordinator::get_proc_rows(const size_t g_nrow,
+        const unsigned nprocs, const unsigned mpi_rank) const {
+    if (mpi_rank == (nprocs - 1)) // The last proc always has more
+        return (g_nrow / nprocs) + (g_nrow % nprocs);
+    else
+        return (g_nrow / nprocs);
+}
 
-void aggregate_clusters() {}
-void reduce_assignment_counts() {}
-void merge_global_assignments() {}
+void dist_task_coordinator::random_partition_init() {
 
+    for (unsigned row = 0; row < nrow; row++) {
+        unsigned asgnd_clust = random() % k; // 0...k
+        const double* dp = this->get_thd_data(row); // TODO: Test get_thd_data
+
+        cltrs->add_member(dp, asgnd_clust);
+        cluster_assignments[row] = asgnd_clust;
+    }
+
+#if VERBOSE
+    printf("After rand paritions cluster_asgns: ");
+    print_arr<unsigned>(cluster_assignments, nrow);
+#endif
+
+}
+
+
+// For testing
+void const dist_task_coordinator::print_thread_data() {
+    std::cout << "\n\nProcess: " << this->mpi_rank;
+    kmeans_task_coordinator::print_thread_data();
+}
+
+void dist_task_coordinator::kmeanspp_init() {}
+void dist_task_coordinator::forgy_init() {}
+void dist_task_coordinator::run_kmeans() {}
 } } // End namespace kpmeans, prune
--- libdist/dist_task_coordinator.hpp
+++ libdist/dist_task_coordinator.hpp
@@ -20,6 +20,7 @@
 #define __KPM_DIST_TASK_COORDINATOR_HPP__
 
 #include "kmeans_task_coordinator.hpp"
+#include "exception.hpp"
 
 namespace kpmeans { namespace prune {
 
@@ -28,18 +29,33 @@ private:
     dist_task_coordinator(const std::string fn, const size_t nrow,
             const size_t ncol, const unsigned k, const unsigned max_iters,
             const unsigned nnodes, const unsigned nthreads,
+            const unsigned mpi_rank, const unsigned nprocs,
             const double* centers, const kpmbase::init_type_t it,
             const double tolerance, const kpmbase::dist_type_t dt);
+
+    unsigned mpi_rank;
+    unsigned nprocs;
+    size_t start_rid_offset; // Per process offset into the file
+
+    /* NOTE
+        nrow: The number of rows LOCAL to the process
+    */
+    size_t const get_proc_rows(const size_t g_nrow,
+            const unsigned nprocs, const unsigned mpi_rank) const;
 
 public:
     static base_kmeans_coordinator::ptr create(
             const std::string fn, const size_t nrow,
             const size_t ncol, const unsigned k, const unsigned max_iters,
             const unsigned nnodes, const unsigned nthreads,
+            const unsigned mpi_rank, const unsigned nprocs,
             const double* centers=NULL, const std::string init="kmeanspp",
             const double tolerance=-1, const std::string dist_type="eucl") {
 
         kpmbase::init_type_t _init_t = kpmbase::get_init_type(init);
+        if (_init_t != kpmbase::init_type_t::RANDOM)
+            throw kpmbase::not_implemented_exception();
+
         kpmbase::dist_type_t _dist_t = kpmbase::get_dist_type(dist_type);
 
 #if KM_TEST
@@ -50,19 +66,18 @@ public:
 #endif
         return base_kmeans_coordinator::ptr(
                 new dist_task_coordinator(fn, nrow, ncol, k, max_iters,
-                    nnodes, nthreads, centers, _init_t, tolerance, _dist_t));
+                    nnodes, nthreads, mpi_rank, nprocs, centers,
+                    _init_t, tolerance, _dist_t));
     }
 
-    // Must override routines
-    virtual void kmeanspp_init();
-    virtual void random_partition_init();
-    virtual void forgy_init();
-    virtual void run_kmeans();
+    const void print_thread_data();
+    std::pair<size_t, size_t> get_rid_len_tup(const unsigned thd_id);
 
-    // Distributed routines
-    void aggregate_clusters();
-    void reduce_assignment_counts();
-    void merge_global_assignments();
+    // Must override routines
+    void kmeanspp_init();
+    void random_partition_init();
+    void forgy_init();
+    void run_kmeans(); /*Run a single iteration*/
 };
 } } // End namespace kpmeans, prune
 #endif
