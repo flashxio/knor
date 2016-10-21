91fe441a78b49ada7a016617c53a74a5de0f8b01
Disa Mhembere <dmhembe1@jhu.edu>
2016-10-21 18:29:15 -0400

    dist: a mini exec / test file

--- exec/dist_kmeans.cpp
+++ exec/dist_kmeans.cpp
@@ -17,9 +17,78 @@
  * limitations under the License.
  */
 
+#include <mpi.h>
+#include <numa.h>
 #include "dist_task_coordinator.hpp"
+#include "clusters.hpp"
+#include "exception.hpp"
+
+static int rank;
+static int nprocs;
+
+namespace kpmeans { namespace mpi {
+
+enum tag_t {
+CLUSTERS, /*For reducing clusters*/
+ASSGN_CNTS, /*For reducing cluster assignment counts*/
+ASSGNMTS, /*For merging assignments at end*/
+};
+
+class driver {
+
+public:
+    static void aggregate_clusters(kpmbase::prune_clusters::ptr
+            , const bool prune) {
+    }
+
+    void reduce_assignment_counts() {}
+    void merge_global_assignments() {}
+};
+}} // namespace kpmeans::mpi
 
 int main(int argc, char* argv[]) {
+    // Setup MPI env
+    if (MPI_Init( &argc, &argv ) != MPI_SUCCESS) {
+        throw std::runtime_error( "MPI_Init error\n");
+    }
 
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); // Set the num_procs
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Create the pthread handler that launches multiple threads
+    std::string datafn = "/data/kmeans/r10_c100_k10_rw.dat";
+    size_t nrow = 100; size_t ncol = 10; size_t k = 5;
+    size_t max_iters = 10; unsigned nnodes = numa_num_task_nodes();
+    size_t nthread = 3; double* p_centers = NULL;
+    std::string init = "random"; double tolerance = -1;
+    std::string dist_type = "eucl";
+
+    if (kpmbase::filesize(datafn.c_str()) != (sizeof(double)*nrow*ncol))
+        throw kpmbase::io_exception("File size does not match input size.");
+#if 1
+    kpmprune::dist_task_coordinator::ptr dc =
+        kpmprune::dist_task_coordinator::create(
+                datafn, nrow, ncol, k, max_iters, nnodes, nthread,
+                rank, nprocs, p_centers, init, tolerance, dist_type);
+#else
+
+#include "kmeans_task_coordinator.hpp"
+    kpmprune::kmeans_task_coordinator::ptr dc =
+        kpmprune::kmeans_task_coordinator::create(
+                datafn, nrow, ncol, k, max_iters, nnodes, nthread, p_centers,
+                init, tolerance, dist_type);
+#endif
+
+    dc->set_global_ptrs();
+    dc->wake4run(kpmeans::thread_state_t::ALLOC_DATA);
+    dc->wait4complete();
+
+    //////////////////////////////////////////////////////////////
+    sleep(rank);
+    dc->print_thread_data();
+    //////////////////////////////////////////////////////////////
+
+    // MPI cleanup and graceful exit
+    MPI_Finalize();
     return EXIT_SUCCESS;
 }
