d36135e7b0e39ce599424bac2c397aba0fbc3f57
Disa Mhembere <dmhembe1@jhu.edu>
2016-10-22 16:15:39 -0400

    exec: can init now -- baby steps

--- exec/dist_kmeans.cpp
+++ exec/dist_kmeans.cpp
@@ -22,12 +22,15 @@
 #include "dist_task_coordinator.hpp"
 #include "clusters.hpp"
 #include "exception.hpp"
+#include "io.hpp"
+
+#define DIST_TEST 1
 
 static int rank;
 static int nprocs;
+static constexpr unsigned root = 0;
 
 namespace kpmeans { namespace mpi {
-
 enum tag_t {
 CLUSTERS, /*For reducing clusters*/
 ASSGN_CNTS, /*For reducing cluster assignment counts*/
@@ -35,16 +38,55 @@ ASSGNMTS, /*For merging assignments at end*/
 };
 
 class driver {
-
 public:
     static void aggregate_clusters(kpmbase::prune_clusters::ptr
-            , const bool prune) {
+            proc_clusters, double* recv_buff) {
+        int ret = MPI_Allreduce(&(proc_clusters->get_means()[0]), recv_buff,
+                proc_clusters->size(), MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        if (ret)
+            throw kpmbase::mpi_exception("All reduce failure on aggregate "
+                    "clusters", ret);
+
+#if DIST_TEST
+        // NOTE: We can check reduce_assignment_counts with this as well
+        //  since the number of members is displayed when means are printed
+        sleep(rank);
+        printf("Proc: %d has per proc clusters: \n", rank);
+        proc_clusters->print_means();
+
+        // Print just to see what we get
+        if (rank == root) {
+            printf("Every proc agg_clust: \n");
+            kpmbase::print_arr<double>(recv_buff, proc_clusters->size());
+        }
+
+#endif
+        // Set new means
+        proc_clusters->set_mean(recv_buff);
     }
 
-    void reduce_assignment_counts() {}
-    void merge_global_assignments() {}
+    static void reduce_assignment_counts(kpmbase::prune_clusters::ptr
+            proc_clusters, long* recv_buff) {
+        int ret = MPI_Allreduce(&(proc_clusters->get_num_members_v()[0]),
+                recv_buff, proc_clusters->get_num_members_v().size(), MPI::LONG,
+                MPI_SUM, MPI_COMM_WORLD);
+        if (ret)
+            throw kpmbase::mpi_exception("All reduce failure on assingnment "
+                    "counts failure", ret);
+        // Set new counts
+        proc_clusters->set_num_members_v(recv_buff);
+
+        // Finalize updated clusters
+        for (unsigned cl_idx = 0;
+                cl_idx < proc_clusters->get_nclust(); cl_idx++)
+            proc_clusters->finalize(cl_idx);
+    }
+
+    void merge_global_assignments() { /*FIXME*/ }
 };
 }} // namespace kpmeans::mpi
+
+namespace kpmmpi = kpmeans::mpi;
 
 int main(int argc, char* argv[]) {
     // Setup MPI env
@@ -65,30 +107,51 @@ int main(int argc, char* argv[]) {
 
     if (kpmbase::filesize(datafn.c_str()) != (sizeof(double)*nrow*ncol))
         throw kpmbase::io_exception("File size does not match input size.");
-#if 1
+
     kpmprune::dist_task_coordinator::ptr dc =
         kpmprune::dist_task_coordinator::create(
                 datafn, nrow, ncol, k, max_iters, nnodes, nthread,
                 rank, nprocs, p_centers, init, tolerance, dist_type);
-#else
-
-#include "kmeans_task_coordinator.hpp"
-    kpmprune::kmeans_task_coordinator::ptr dc =
-        kpmprune::kmeans_task_coordinator::create(
-                datafn, nrow, ncol, k, max_iters, nnodes, nthread, p_centers,
-                init, tolerance, dist_type);
-#endif
 
     dc->set_global_ptrs();
     dc->wake4run(kpmeans::thread_state_t::ALLOC_DATA);
     dc->wait4complete();
 
+    struct timeval start, end;
+    gettimeofday(&start , NULL);
     //////////////////////////////////////////////////////////////
-    sleep(rank);
-    dc->print_thread_data();
+    if (init != "random")
+        throw kpmbase::not_implemented_exception();
+
+    double* clstr_buff = new double[k*ncol];
+    long* nmemb_buff = new long[k];
+
+    dc->run_init();
+
+    // TODO: Check cost of all the shared_ptr passing
+    kpmbase::prune_clusters::ptr cls_ptrs = std::static_pointer_cast<
+        kpmprune::dist_task_coordinator>(dc)->get_gcltrs();
+    kpmmpi::driver::aggregate_clusters(cls_ptrs, clstr_buff);
+    kpmmpi::driver::reduce_assignment_counts(cls_ptrs, nmemb_buff);
+
+#if DIST_TEST
+    BOOST_VERIFY((size_t)std::accumulate(cls_ptrs->get_num_members_v().begin(),
+                cls_ptrs->get_num_members_v().end(), 0) == nrow);
+
+    if (rank == root) {
+        printf("New finalized centers for Proc: %d ==> \n", rank);
+        cls_ptrs->print_means();
+    }
+#endif
     //////////////////////////////////////////////////////////////
 
+    if (rank == root)
+        BOOST_LOG_TRIVIAL(info) << "\n\nAlgorithmic time taken = " <<
+            kpmbase::time_diff(start, end) << " sec\n";
+
     // MPI cleanup and graceful exit
+    delete [] clstr_buff;
+    delete [] nmemb_buff;
     MPI_Finalize();
     return EXIT_SUCCESS;
 }
