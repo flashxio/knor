2e2c4dec2df0ed19003f911939ad0d59aa580c4f
Disa Mhembere <dmhembe1@jhu.edu>
2016-11-07 01:20:30 -0500

    common: kpm common return type

--- libcommon/kmeans_types.hpp
+++ libcommon/kmeans_types.hpp
@@ -21,12 +21,40 @@
 #define __KPM_TYPES_HPP__
 
 #include <limits>
+#include <vector>
+#include "io.hpp"
 
 namespace kpmeans { namespace base {
-    static const unsigned INVALID_CLUSTER_ID =
-        std::numeric_limits<unsigned>::max();
-    enum kms_stage_t { INIT, ESTEP }; // What phase of the algo we're in
-    enum dist_type_t { EUCL, COS }; // Euclidean, Cosine distance
-    enum init_type_t { RANDOM, FORGY, PLUSPLUS, NONE }; // May have to use
+
+static const unsigned INVALID_CLUSTER_ID = std::numeric_limits<unsigned>::max();
+enum kms_stage_t { INIT, ESTEP }; // What phase of the algo we're in
+enum dist_type_t { EUCL, COS }; // Euclidean, Cosine distance
+enum init_type_t { RANDOM, FORGY, PLUSPLUS, NONE }; // May have to use
+
+template <typename T>
+class kmeans_t {
+public:
+    std::vector<unsigned> assignments;
+    std::vector<size_t> assignment_count;
+    size_t iters;
+    std::vector<T> centroids;
+
+    kmeans_t(std::vector<unsigned>& assignments,
+            size_t* assignment_count_buf, const size_t k,
+            const size_t iters, std::vector<T>& centroids) {
+        this->assignments = assignments;
+        this->iters = iters;
+        this->assignment_count.resize(k);
+        std::copy(assignment_count_buf, assignment_count_buf + k,
+                assignment_count.begin());
+        this->centroids = centroids;
+    }
+
+    const void print() const {
+        std::cout << "Iterations: " <<  iters << std::endl;
+        std::cout << "Cluster count: ";
+        kpmeans::base::print_vector(assignment_count);
+    }
+};
 } }
 #endif
