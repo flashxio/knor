21c4a2bf83a8c9bc9c8e04849112585eacaef45a
Disa Mhembere <dmhembe1@jhu.edu>
2017-01-25 05:39:43 -0500

    use: attempt to push to dockerhub on a successful travis build

---
+++ push_docker.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Copyright 2016 neurodata (http://neurodata.io/)
+# Written by Disa Mhembere (disa@jhu.edu)
+#
+# This file is part of k-par-means.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+  docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PWD
+  export REPO=disamhembere/knor
+  export TAG=latest
+  docker build -f Dockerfile -t $REPO:$COMMIT .
+  docker tag $REPO:$COMMIT $REPO:$TAG
+  #docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+  docker push $REPO
+fi
