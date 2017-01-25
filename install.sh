d2a64b46f83ec902b7ef98ec51e8a66934cbeda3
Disa Mhembere <dmhembe1@jhu.edu>
2017-01-25 03:31:06 -0500

    bug: install error

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@
 cd $HOME
 # Make sure the package information is up-to-date
 apt-get update
-apt-get -o Dpkg::Options::="--force-confold" --force-yes -y upgrade
+apt-get sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y upgrade
 
 apt-get -y install build-essential
 # In memory dependencies
