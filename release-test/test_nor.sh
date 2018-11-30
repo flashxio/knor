99d6ec35fcf8a4b726da4a39a43505e6afab3d4b
Disa Mhembere <dmhembe1@jhu.edu>
2018-11-30 03:43:22 -0500

    test: bug in test_nor

--- release-test/test_nor.sh
+++ release-test/test_nor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Testing medoids"
-cd .. &&
+cd .. && \
     exec/medoids -f test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 && \
     echo "Testing skmeans" && \
     exec/skmeans -f test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 && \
