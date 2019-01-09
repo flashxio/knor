a4a342941d52626a710876a70fda6036847e08db
Disa Mhembere <dmhembe1@jhu.edu>
2019-01-08 23:43:08 -0500

    test: test_nor update

--- release-test/test_nor.sh
+++ release-test/test_nor.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 echo "Testing medoids"
-cd .. && \
-    exec/medoids -f test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 && \
-    echo "Testing skmeans" && \
-    exec/skmeans -f test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 && \
-    echo "Testing fcm" && \
-    exec/fcm -f test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 #&& \
-    #echo "Testing mb_knori" && \
-    #exec/mb_knori -f ../test-data/iris.bin -n 150 -m 4 -k 3 -T 2 -i 10 -M 30
+../exec/medoids -f ../test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 && \
+echo "Testing skmeans" && \
+../exec/skmeans -f ../test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 && \
+echo "Testing fcm" && \
+../exec/fcm -f ../test-data/iris.bin -n 150 -m 4 -k 3 -T 32 -i 10 #&& \
+#echo "Testing mb_knori" && \
+#exec/mb_knori -f ../test-data/iris.bin -n 150 -m 4 -k 3 -T 2 -i 10 -M 30
