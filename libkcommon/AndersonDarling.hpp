a135cf60dc81582321b5259e93b0400ebcbe5677
Disa Mhembere <dmhembe1@jhu.edu>
2019-01-31 03:47:39 -0500

    Anderson Darling statistic & test

---
+++ libkcommon/AndersonDarling.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2016 neurodata (http://neurodata.io/)
+ * Written by Disa Mhembere (disa@jhu.edu)
+ *
+ * This file is part of knor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY CURRENT_KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __KNOR_ANDERSON_DARLING_HPP__
+#define __KNOR_ANDERSON_DARLING_HPP__
+
+#include <cmath>
+#include <algorithm>
+
+namespace knor { namespace base {
+
+class AndersonDarling {
+private:
+	static const double phi(const double x) {
+		return 0.5 * erfc(-x * M_SQRT1_2);
+	}
+
+public:
+	static void compute_critical_values(double N, std::vector<double>& out) {
+		std::vector<double> _Avals_norm { 0.576, 0.656, 0.787, 0.918, 1.092 };
+		const double divisor = 1.0 + 4.0/N - 25.0/(N*N);
+		for (auto const& i : _Avals_norm) {
+			out.push_back(i / divisor);
+		}
+	}
+
+	static double compute_statistic(const int n, double X[]) {
+		std::sort(X, X + n);
+		// Find the mean of X
+		double X_avg = 0.0;
+		double sum = 0.0;
+		for (int i = 0; i < n; i++) {
+			sum += X[i];
+		}
+		X_avg = sum/static_cast<double>(n);
+
+
+		// Find the variance of X
+		double X_sig = 0.0;
+		for (int i = 0; i < n; i++) {
+			X_sig += (X[i] - X_avg)*(X[i] - X_avg);
+		}
+		X_sig /= (n-1);
+
+
+		// The values X_i are standardized to create new values Y_i
+		double Y[n];
+		for (int i = 0; i < n; i++) {
+			Y[i] = (X[i] - X_avg)/(std::sqrt(X_sig));
+		}
+
+		// With a standard normal CDF, we calculate the Anderson_Darling Statistic
+		double A = -n;
+		for(int i = 0; i < n; i++) {
+			A +=  -1.0 / static_cast<double>(n) *(2*(i + 1) - 1) *
+				(std::log(phi(Y[i])) + std::log(1 - phi(Y[n - 1 - i])));
+		}
+
+		return A;
+	}
+
+	static void compute_statistic(std::vector<double>& X) {
+		compute_statistic(X.size(), &X[0]);
+	}
+};
+
+}} // End namespace knor::base
+#endif
---
+++ libkcommon/unit-test/test_AD.cpp
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2016 neurodata (http://neurodata.io/)
+ * Written by Disa Mhembere (disa@jhu.edu)
+ *
+ * This file is part of knor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <vector>
+#include <cassert>
+#include <stdio.h>
+
+#include "io.hpp"
+#include "AndersonDarling.hpp"
+
+using namespace knor::base;
+
+int main() {
+	std::vector<double> x {1, 2, 3, 4};
+
+	const double EPS = 0.00001;
+
+	auto ad = AndersonDarling::compute_statistic(4, &x[0]);
+	const double AD_RES = 0.159201;
+	printf("AD: %.5f\n", ad);
+	assert(ad - AD_RES < EPS);
+
+	std::vector<double> cv;
+	std::vector<double> CV_RES {1.31657, 1.49943, 1.79886, 2.09829, 2.496};
+	AndersonDarling::compute_critical_values(x.size(), cv);
+
+	printf("Critical values: \n");
+	print(cv);
+
+	assert(cv.size() == CV_RES.size());
+	for (size_t i = 0; i < cv.size(); i++)
+		assert(cv[i] - CV_RES[i] < EPS);
+
+	printf("\nAndersonDarling test successful!\n");
+}
