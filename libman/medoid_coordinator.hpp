bbb798f1d6a5bb3df7ca2456163da84899fe4f53
Disa Mhembere <dmhembe1@jhu.edu>
2019-06-03 02:11:03 -0400

    default arg for medoids incorrect

--- libman/medoid_coordinator.hpp
+++ libman/medoid_coordinator.hpp
@@ -55,7 +55,7 @@ class medoid_coordinator : public coordinator {
                 const size_t nrow,
                 const size_t ncol, const unsigned k, const unsigned max_iters,
                 const unsigned nnodes, const unsigned nthreads,
-                const double* centers=NULL, const std::string init="random",
+                const double* centers=NULL, const std::string init="forgy",
                 const double tolerance=-1, const std::string dist_type="taxi",
                 const double sample_rate=.2) {
 
